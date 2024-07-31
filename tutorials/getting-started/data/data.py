78f95777788570727c374a12276d19816ebd470f
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-07-31 16:51:34 +0800

    refactor: data.py

--- tutorials/getting-started/data/data.py
+++ tutorials/getting-started/data/data.py
@@ -4,13 +4,10 @@ import pandas as pd
 
 dir = path.dirname(__file__)
 
-__csv_files = {
-    "titanic": "titanic.csv",
-}
-
 
 def __read_file(name: str) -> pd.DataFrame:
-    return pd.read_csv(path.join(dir, __csv_files[name]))
+    return pd.read_csv(path.join(dir, name))
 
 
-titanic = __read_file("titanic")
+air_quality = __read_file("air_quality_no2.csv")
+titanic = __read_file("titanic.csv")
