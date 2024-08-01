22a59d953848e5cf9776003884b386587e101a73
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-08-01 23:21:42 +0800

    chore: data for sections

--- tutorials/getting-started/data/data.py
+++ tutorials/getting-started/data/data.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 dir = path.dirname(__file__)
 
-# output data
+# data for sections
 titanic = pd.read_csv(path.join(dir, "titanic.csv"))
 air_quality = pd.read_csv(path.join(dir, "air_quality_no2.csv"),
                           index_col=0, parse_dates=True)
