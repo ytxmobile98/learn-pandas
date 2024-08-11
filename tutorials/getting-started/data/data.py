a23a55ab253d02c0991ae44488d04aafdc198b92
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-08-11 14:09:30 +0800

    feat: combine data frames

--- tutorials/getting-started/data/data.py
+++ tutorials/getting-started/data/data.py
@@ -10,3 +10,10 @@ air_quality = pd.read_csv(path.join(dir, "air_quality_no2.csv"),
                           index_col=0, parse_dates=True)
 air_quality_long = pd.read_csv(path.join(dir, "air_quality_long.csv"),
                                index_col="date.utc", parse_dates=True)
+air_quality_no2_long = pd.read_csv(path.join(dir, "air_quality_no2_long.csv"),
+                                   parse_dates=True)
+air_quality_pm25 = pd.read_csv(path.join(dir, "air_quality_pm25_long.csv"),
+                               parse_dates=True)
+stations_coord = pd.read_csv(path.join(dir, "air_quality_stations.csv"))
+air_quality_parameters = pd.read_csv(path.join(dir,
+                                               "air_quality_parameters.csv"))
