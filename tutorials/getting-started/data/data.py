d453212789642deee4630d922386026e11da86cd
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-08-11 13:30:30 +0800

    feat: reshape table layout

--- tutorials/getting-started/data/data.py
+++ tutorials/getting-started/data/data.py
@@ -8,3 +8,5 @@ dir = path.dirname(__file__)
 titanic = pd.read_csv(path.join(dir, "titanic.csv"))
 air_quality = pd.read_csv(path.join(dir, "air_quality_no2.csv"),
                           index_col=0, parse_dates=True)
+air_quality_long = pd.read_csv(path.join(dir, "air_quality_long.csv"),
+                               index_col="date.utc", parse_dates=True)
